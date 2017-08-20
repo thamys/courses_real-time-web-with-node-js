9e8b7b8f3146b86420e79313654c8204d7cb8fd0
Thamyres Corrêa <thataguedis@gmail.com>
2017-08-19 23:23:54 -0300

    Testnh non-blocking

--- hello.js
+++ hello.js
@@ -2,7 +2,10 @@ var http = require('http');
 
 http.createServer(function(request, response){
     response.writeHead(200);
-    response.write("Hello, this is dog");
-    response.end();
+    response.write("Dog is running.");
+    setTimeout(function(){
+        response.write("Dog is done");
+        response.end();
+    }, 5000)
 }).listen(8080);
 console.log('Listening on port 8080..');
