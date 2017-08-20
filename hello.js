3c9b16e4eb1d5176c66b3927619e608c550c8f06
Thamyres Corrêa <thataguedis@gmail.com>
2017-08-19 23:40:34 -0300

    refactoring.

--- hello.js
+++ hello.js
@@ -1,11 +1,16 @@
 var http = require('http');
 
-http.createServer(function(request, response){
+var server = http.createServer();
+
+server.on('request', function(request, response){
     response.writeHead(200);
     response.write("Dog is running.");
     setTimeout(function(){
         response.write("Dog is done");
         response.end();
     }, 5000)
-}).listen(8080);
+});
+
+
+server.listen(8080);
 console.log('Listening on port 8080..');
