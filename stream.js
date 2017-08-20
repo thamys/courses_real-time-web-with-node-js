4a5a2e9aae71b20eafdb44d768f93d94b793e0c8
Thamyres Corrêa <thataguedis@gmail.com>
2017-08-19 23:58:54 -0300

    streaming data

--- stream.js
+++ stream.js
@@ -1,17 +1,8 @@
 var http = require('http');
 
-http.createServer.on('request', function(request, response){
+http.createServer(function(request, response){
     response.writeHead(200);
-    request.on('readdable', function(){
-        var chunk = null;
-        while(null !== (chunk == request.read())) {
-            response.write(chunk.toString());
-        }
-    });
-
-    request.on('end', function(){
-        response.end();
-    });
+    request.pipe(response);
 }).listen(8080);
 
 console.log('Listening on port 8080..');
